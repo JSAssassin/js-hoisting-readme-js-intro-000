96a56c611c4958c2f43c63d78db5b3ba13a64b4c
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 02:39:35 +0000

    Automatically backed up by Learn

--- scratch.js
+++ scratch.js
@@ -1,13 +1,12 @@
-var testV;
-
-console.log(testV);
-
-testV = test();
-
-
 function test() {
   var hello = 0;
   console.log("this does stuff");
 
   return 42;
 }
+
+var testV;
+
+console.log(testV);
+
+testV = test();
